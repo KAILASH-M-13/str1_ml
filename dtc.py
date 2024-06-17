45d660c558ada7e5786c67e9d61f75e945b08d0e
KAILASH-M-13 <110119183+KAILASH-M-13@users.noreply.github.com>
2024-06-17 15:47:59 +0530

    Update dtc.py

--- dtc.py
+++ dtc.py
@@ -19,4 +19,9 @@ if csv_data :
 
         if submit:
           st.session_state['user'].append(user_input)
+          st.session_state['assistant'].append(output)
+if st.session_state['assistant']:
+        for i in range(len(st.session_state['assistant'])):
+            message(st.session_state["user"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["assistant"][i], key=str(i))
             
